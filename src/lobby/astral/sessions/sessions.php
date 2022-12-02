0ac3b5f43464456ce15b1189448a453ac8616484
ImJust1one <119627450+ImJust1one@users.noreply.github.com>
2022-12-01 23:12:50 -0400

    Sessions Create

--- src/lobby/astral/sessions/sessions.php
+++ src/lobby/astral/sessions/sessions.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace lobby\astral\sessions;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class sessions implements Listener {
+
+  public function onJoin(PlayerJoinEvent $event){
+
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    $event->setJoinMessage("§7[§9+§7] §9" . $name);
+    
+   }
+    
+   public function onQuit(PlayerQuitEvent $event){
+    
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    $event->setQuitMessage("§7[§c-§7] §c" . $name);
+    
+   }
+
+
+}
