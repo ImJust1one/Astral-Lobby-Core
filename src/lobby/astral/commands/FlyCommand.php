bc0d7979b12ebd2635784992b916dbae124e950c
ImJust1one <119627450+ImJust1one@users.noreply.github.com>
2022-12-02 12:24:08 -0400

    FlyCommand Create

--- src/lobby/astral/commands/FlyCommand.php
+++ src/lobby/astral/commands/FlyCommand.php
@@ -1,1 +1,44 @@
+<?php
+
+namespace lobby\astral\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+
+class FlyCommand extends Command{
+
+  public function __construct(){
+    parent::__construct("Fly", "U wanna fly?");
+    $this->setpermission('fly.command');
+    
+  }
+
+  public function execute(CommandSender $sender, string $commandLabel, array $args){
+    if ($sender->haspermission("fly.command")){
+        $sender->SendMessage("the pussy's Cant Fly");
+        return;
+    }
+    if(!isset($args[0])){
+        $sender->SendMessage("learn to write stupid monkey, /fly on, /fly off");
+        return;
+    }
+    
+
+    if($args[0] == "on"){
+    $sender->setFlying(true);
+    $sender->SendMessage("U're flying :D");
+
+    }
+    
+    if($args[0] == "off"){
+    $sender->SendMessage("Ok.");
+    $sender->setFlying(false);
+
+    }
+    
+  }
+
+}
+
 
